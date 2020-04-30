8a2463deebb021bd7b9c26b6909c5650897701c7
Achal1607 <talatiachal16@gmail.com>
2020-04-30 23:56:11 +0530

    Google Login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -63,12 +63,13 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-success">
                                     {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
-                    </form>
+                    </form><div class="col-md-6 offset-md-4 pt-md-4 font-weight-bolder font" >Or</div>
+                    <div class="col-md-6 offset-md-4 pt-md-4" style="cursor: pointer"><a href="/redirect"><img src="/svg/google.png" alt="google sign in"></a></div>
                 </div>
             </div>
         </div>
